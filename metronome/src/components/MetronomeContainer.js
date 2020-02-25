a86e791ce0392805695b31336ebf7060a4cde46f
Steve <stevekentlanier@gmail.com>
2020-02-25 13:43:01 -0800

    more changes

---
+++ metronome/src/components/MetronomeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MetronomeContainer extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+   
+}
+}
+
+export default MetronomeContainer;
